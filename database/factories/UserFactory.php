bd1e9982a2ae53847e70cf173d73b5703b62f6fc
syaafiudinm <altafpasallo12@gmail.com>
2026-02-12 22:43:35 +0800

    fix for production

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => 'testttt',
-            'email' => 'email@example.com',
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            "name" => "testttt",
+            "email" => "email@example.com",
+            "email_verified_at" => now(),
+            "password" => (static::$password ??= Hash::make("password")),
+            "remember_token" => Str::random(10),
         ];
     }
 
@@ -37,8 +37,10 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        return $this->state(
+            fn(array $attributes) => [
+                "email_verified_at" => null,
+            ],
+        );
     }
 }
