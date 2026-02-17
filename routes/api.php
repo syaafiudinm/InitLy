e64ee5c017f4c76e7963fabf1c4bb852e930ea39
syaafiudinm <altafpasallo12@gmail.com>
2026-02-18 02:46:41 +0800

    build for prod

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\StarterKitController;
+use App\Http\Controllers\API\StarterKitController;
 
 Route::get("/user", function (Request $request) {
     return $request->user();
